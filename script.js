69affecbb2bf8ea6b4be0b542514e9b0287498e9
CaptainSkiddow <dekkerrens@outlook.com>
2024-03-21 14:20:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const todoList = document.querySelector('#todos');
+
+const todos = [
+  { text: 'Order cat food', completed: false },
+  { text: 'Clean kitchen', completed: true },
+  { text: 'Buy food', completed: true },
+  { text: 'Do work', completed: false },
+  { text: 'Exercise', completed: true }
+];
+
+// loop through the todos array and create a new p for each todo
+todos.forEach(function (todo) {
+  const todoElement = document.createElement('li');
+  todoElement.textContent = todo.text;
+  todoList.appendChild(todoElement);
+});
+
+// ik wil todo's kunnen toevoegen
+// een onSubmit event die afvangen
+// de value van een input pakken
+// dat toevoegen aan de array
+// dan moet de render todo's gebeuren
+// dus misscien wel een functie die todo's rendert
+// een functie een todo aanmaakt?
+// en wanneer er 1tje is gemaakt, dan de functie van renderen aanroepen.
+
+// ik wil todos kunnen verwijderen
+
+// ik wil de status van de todo kunnen veranderen, completed, true false
+
